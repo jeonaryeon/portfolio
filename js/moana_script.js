c0dbcd4c5895a6424300c79535d35d12664e03e6
jeonaryeon <aryeon96@naver.com>
2020-01-07 21:56:57 +0900

    portfolio
    portfolio

--- js/moana_script.js
+++ js/moana_script.js
@@ -1,13 +1,31 @@
 $(function(){
   var introH=$('#intro').height();
+  var wH=$(window).height();
+  var wTop=$(window).scrollTop();
+  
   $(window).on('scroll',function(){
     var wTop=$(window).scrollTop();
-    if(wTop>=introH){
+    if(wTop>=introH-100){
       $('header').css({opacity:'1'});
     }else{
       $('header').css({opacity:0});
     }
+    
+    for(var idx=0; idx<5; idx++){
+      if(wTop>=(wH*idx)-200 && wTop<wH*(idx+1)){
+        $('#gnb>li').removeClass('on');
+        $('#gnb>li').eq(idx).addClass('on');
+      }
+    }
   }); // end of scroll
+  
+  $('#gnb>li').on('click',function(){
+    var idx=$(this).index();
+    var top=wH*idx;
+    $('html,body').stop().animate({scrollTop:top},500);
+    $(this).addClass('on').siblings().removeClass('on');
+  }); // end of #gnb
+  
   
   $('#intro').on('mousemove',function(e){
     var posX=e.pageX;
@@ -16,6 +34,7 @@ $(function(){
     $('.water1').css({left:400-(posX/50),bottom:-160+(posY/70)});
     $('.water2').css({left:80+(posX/70),bottom:-180-(posY/50)});
   }); // end of #intro mousemove
+  
   
   $('#intro>button').on('click',function(){
     $('html,body').stop().animate({scrollTop:introH});
